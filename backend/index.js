d798c1c40c226c466377174c27b45090b5e87c9e
Ashwin3207 <amathura01@gmail.com>
2024-12-20 12:56:48 +0530

    changes

---
+++ backend/index.js
@@ -0,0 +1,40 @@
+import express from "express";
+import dotenv from "dotenv";
+import bodyParser from "body-parser";
+import cors from "cors";
+import userRoutes from "./routes/userRoutes.js";
+import mongoose from "mongoose";
+
+dotenv.config();
+
+const app = express();
+
+// Set up the database URL from environment variable
+const dbUrl = process.env.ATLASDB_URL;
+
+// Connect to the database
+main()
+  .then(() => {
+    console.log("Connected to database");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+async function main() {
+  await mongoose.connect(dbUrl);
+}
+
+
+
+// Middleware
+app.use(cors());
+app.use(bodyParser.json());
+app.use("/api/users", userRoutes); // Your user routes for login and OTP
+
+// Start server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+
+  console.log(`Server running on port ${PORT}`);
+});
